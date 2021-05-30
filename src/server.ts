2d1b1399d4e7fdb272bab2c95a168b81b64fcb86
Artem <artem@MacBook-Pro-Artem.local>
2021-05-30 21:04:52 +0300

    feat: ts app run

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 const { PORT } = require('./common/config');
-const app = require('./app');
+const app1 = require('./app');
 
-app.listen(PORT, () =>
+app1.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 );
