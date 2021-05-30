77fb1a6e42c78195f73aa7352775e30400527484
Artem <artem@MacBook-Pro-Artem.local>
2021-05-30 14:58:39 +0300

    docs: update description of user.service function

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -11,26 +11,53 @@ const UserSchema = Joi.object({
     id: Joi.string()
 });
 /**
- * The function returns the function "get all users"
- * @returns {function} function usersRepo.getAll()
+ * Calling the function "get all users"
+ * @returns {getAll~usersRepo.getAll()} the returned function
  */
 const getAll = () => usersRepo.getAll();
 
+/**
+ * Calling the function "get user by id"
+ * @param {string} id User ID
+ * @returns {getUser~usersRepo.getUser(id)} the returned function
+ */
 const getUser = (id) => usersRepo.getUser(id);
 
+/**
+ * Calling the function "create user"
+ * @param {object} user User object
+ * @returns {createUser~usersRepo.createUser(user)} the returned function
+ */
 const createUser = (user) => {
     usersRepo.createUser(user);
 };
 
+/**
+ *  User object verification by scheme
+ * @param {object} user User object
+ * @returns {boolean} True if it matches the scheme
+ */
 const validateUser = (user) =>{
     const result = UserSchema.validate(user);
     if (result.error) {
         throw new Error(`неверный формат данных`);
     }
+    return true;
 } 
 
+/**
+ * Calling the function update user
+ * @param {string} id User Id
+ * @param {object} data User data
+ * @returns {updateUser~usersRepo.updateUser(id,data)} the returned function
+ */
 const updateUser = (id, data) => usersRepo.updateUser(id,data);
 
+/**
+ * Calling the function delete user
+ * @param {string} id User Id 
+ * @returns {deleteUser~usersRepo.deleteUser(id)} the returned function
+ */
 const deleteUser = (id) => usersRepo.deleteUser(id);
 
 module.exports = { getAll, createUser, validateUser, getUser, updateUser, deleteUser};
